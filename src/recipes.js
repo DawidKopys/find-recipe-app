fa0409821b50184ac172b19fb872c8915d7ec7e5
Dewke <dewke17@gmail.com>
2021-02-22 10:57:10 +0100

    Recipe cards improve styling

--- src/recipes.js
+++ src/recipes.js
@@ -7,24 +7,24 @@ const recipes = [
     macro: { calories: 600, protein: 20, carbs: 30, fats: 15 },
     image: PorrigeImage,
   },
-  // {
-  //   id: 2,
-  //   name: 'Peanut Butter Porrige',
-  //   macro: { calories: 600, protein: 20, carbs: 30, fats: 15 },
-  //   image: PorrigeImage,
-  // },
-  // {
-  //   id: 3,
-  //   name: 'Peanut Butter Porrige',
-  //   macro: { calories: 600, protein: 20, carbs: 30, fats: 15 },
-  //   image: PorrigeImage,
-  // },
-  // {
-  //   id: 4,
-  //   name: 'Peanut Butter Porrige',
-  //   macro: { calories: 600, protein: 20, carbs: 30, fats: 15 },
-  //   image: PorrigeImage,
-  // },
+  {
+    id: 2,
+    name: 'Peanut Butter Porrige',
+    macro: { calories: 600, protein: 20, carbs: 30, fats: 15 },
+    image: PorrigeImage,
+  },
+  {
+    id: 3,
+    name: 'Peanut Butter Porrige',
+    macro: { calories: 600, protein: 20, carbs: 30, fats: 15 },
+    image: PorrigeImage,
+  },
+  {
+    id: 4,
+    name: 'Peanut Butter Porrige',
+    macro: { calories: 600, protein: 20, carbs: 30, fats: 15 },
+    image: PorrigeImage,
+  },
 ];
 
 export default recipes;
