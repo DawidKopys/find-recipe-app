d2eefb4dab12df58a342f53302eb7c26026c5a3a
Dewke <dewke17@gmail.com>
2021-02-28 11:34:56 +0100

    Extract RecipesList into separate component

--- src/FindRecipeApp.js
+++ src/FindRecipeApp.js
@@ -2,56 +2,13 @@ import React, { useState } from 'react';
 import './FindRecipeApp.css';
 import recipes from './recipes';
 import Sidebar from './Sidebar/Sidebar';
+import RecipesList from './RecipesList/RecipesList';
 
 const FindRecipeApp = () => {
   return (
     <div className='recipe-finder-app'>
       <Sidebar />
-      <section className='recipes-container'>
-        {recipes.map((recipe) => {
-          const {
-            id,
-            name,
-            macro: { calories, protein, carbs, fats },
-            imageMobile,
-            imageDesktop,
-          } = recipe;
-          return (
-            <div className='recipe-card' key={id}>
-              <img
-                srcSet={`${imageMobile} 105w, ${imageDesktop} 640w`}
-                sizes='(max-width: 800px) 105px, 150px'
-                src={imageMobile}
-                alt={name}
-              />
-              <div className='recipe-info-container'>
-                <h3>{name}</h3>
-                <div className='recipe-macro-container'>
-                  <p className='recipe-macro-per-serving'>Macro per serving:</p>
-                  <div className='recipe-macro'>
-                    <div className='recipe-macro-detail'>
-                      <h4>{calories}</h4>
-                      <p>kcal</p>
-                    </div>
-                    <div className='recipe-macro-detail'>
-                      <h4>{protein}</h4>
-                      <p>protein</p>
-                    </div>
-                    <div className='recipe-macro-detail'>
-                      <h4>{carbs}</h4>
-                      <p>carbs</p>
-                    </div>
-                    <div className='recipe-macro-detail'>
-                      <h4>{fats}</h4>
-                      <p>fats</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </section>
+      <RecipesList recipes={recipes} />
     </div>
   );
 };
---
+++ src/RecipesList/RecipesList.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './RecipesList.css';
+
+const RecipesList = ({ recipes }) => {
+  return (
+    <section className='recipes-container'>
+      {recipes.map((recipe) => {
+        const {
+          id,
+          name,
+          macro: { calories, protein, carbs, fats },
+          imageMobile,
+          imageDesktop,
+        } = recipe;
+        return (
+          <div className='recipe-card' key={id}>
+            <img
+              srcSet={`${imageMobile} 105w, ${imageDesktop} 640w`}
+              sizes='(max-width: 800px) 105px, 150px'
+              src={imageMobile}
+              alt={name}
+            />
+            <div className='recipe-info-container'>
+              <h3>{name}</h3>
+              <div className='recipe-macro-container'>
+                <p className='recipe-macro-per-serving'>Macro per serving:</p>
+                <div className='recipe-macro'>
+                  <div className='recipe-macro-detail'>
+                    <h4>{calories}</h4>
+                    <p>kcal</p>
+                  </div>
+                  <div className='recipe-macro-detail'>
+                    <h4>{protein}</h4>
+                    <p>protein</p>
+                  </div>
+                  <div className='recipe-macro-detail'>
+                    <h4>{carbs}</h4>
+                    <p>carbs</p>
+                  </div>
+                  <div className='recipe-macro-detail'>
+                    <h4>{fats}</h4>
+                    <p>fats</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
+export default RecipesList;
