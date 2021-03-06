222c7373339070cce5f32544eca79fc800911d34
Dewke <dewke17@gmail.com>
2021-03-06 15:26:53 +0100

    Mobiles: make scrolling to top smooth with requestAnimationFrame

--- src/RecipesSection/RecipesList/RecipesList.js
+++ src/RecipesSection/RecipesList/RecipesList.js
@@ -3,9 +3,18 @@ import './RecipesList.css';
 import RecipeCard from './RecipeCard/RecipeCard';
 
 const RecipesList = ({ recipes }) => {
-  const scrolltoTop = () => {
-    document.body.scrollTop = 0; /* For Safari */
-    document.documentElement.scrollTop = 0; /* For Chrome, Firefox, IE and Opera */
+  const smoothScrollToTop = () => {
+    const currentScroll =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentScroll > 0) {
+      window.requestAnimationFrame(smoothScrollToTop);
+      document.body.scrollTop =
+        currentScroll - currentScroll / 5; /* For Safari */
+      document.documentElement.scrollTop =
+        currentScroll -
+        currentScroll / 5; /* For Chrome, Firefox, IE and Opera */
+    }
   };
 
   return (
@@ -17,7 +26,7 @@ const RecipesList = ({ recipes }) => {
       </div>
       <button
         className='btn-scroll-to-top recipes-container__btn-scroll-to-top'
-        onClick={scrolltoTop}
+        onClick={smoothScrollToTop}
       ></button>
     </main>
   );
