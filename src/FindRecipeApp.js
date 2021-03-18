01297275c7c9f12994a4fc969ea1ca84662c3a35
Dewke <dewke17@gmail.com>
2021-03-18 21:50:39 +0100

    Navbar - initial markup and styling

--- src/FindRecipeApp.js
+++ src/FindRecipeApp.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './FindRecipeApp.css';
+import Navbar from './Navbar/Navbar';
 import Sidebar from './Sidebar/Sidebar';
 import RecipesSection from './RecipesSection/RecipesSection';
 
 const FindRecipeApp = () => {
   return (
     <div className='recipe-finder-app'>
+      <Navbar />
       <Sidebar />
       <RecipesSection />
     </div>
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Logo from 'Assets/logo-reciply-purple.svg';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar recipe-finder-app__navbar'>
+      <img src={Logo} alt='Reciply Logo' className='navbar__logo' />
+      <ul className='navbar__links-list'>
+        <li className='navbar__list-item'>
+          <a href='' className='navbar__link'>
+            Add Recipe
+          </a>
+        </li>
+        <li className='navbar__list-item'>
+          <a href='' className='navbar__link'>
+            Find Recipe
+          </a>
+        </li>
+        <li className='navbar__list-item'>
+          <a href='' className='navbar__link navbar__link--sign-in'>
+            Sign In
+          </a>
+        </li>
+        <li className='navbar__list-item'>
+          <a href='' className='navbar__link navbar__link--sign-up'>
+            Sign Up
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
