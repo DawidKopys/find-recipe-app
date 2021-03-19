d2209b55b14200cdbc10baf3719e9a3a9ff02ecf
Dewke <dewke17@gmail.com>
2021-03-19 12:57:27 +0100

    Navbar - initial markup and styling for mobiles

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Logo from 'Assets/logo-reciply-purple-uppercase.svg';
+import BurgerMenu from 'Assets/icon-burger-menu-grey.svg';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className='navbar recipe-finder-app__navbar'>
-      <img src={Logo} alt='Reciply Logo' className='navbar__logo' />
+      <img src={Logo} alt='Reciply Logo' className='navbar__logo-img' />
+      <button className='navbar--burger-menu'>
+        <img src={BurgerMenu} alt='Show Menu' />
+      </button>
       <ul className='navbar__links-list'>
         <li className='navbar__list-item'>
           <a href='' className='navbar__link'>
